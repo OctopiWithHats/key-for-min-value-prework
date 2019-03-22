b091468cfb386953a29bc7494dca9e5f65ce9c2d
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 21:19:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
   
-  name_hash.each do |k, v|
+  name_hash.each do |key, value|
     if min_value == nil
       min_value = v
       min_key = k
